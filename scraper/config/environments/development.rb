3f78370b3a24d1871eaa39eabf686ee9c55cd126
Alikk-a <albert@navigatorway.com>
2024-01-31 21:36:05 +0100

    complete basec version

--- scraper/config/environments/development.rb
+++ scraper/config/environments/development.rb
@@ -68,8 +68,4 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  # Rails.logger = Logger.new('log/scraper.log')
-  # Rails.logger.level = Logger::WARN
-  # Rails.logger.datetime_format = "%Y-%m-%d %H:%M:%S"
-
 end
